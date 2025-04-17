b6adc9914e9bdc9f36b817c80be8343451b6f952
Radies <ranyoung0102@gmail.com>
2025-04-17 10:38:48 +0900

    git add & commit

---
+++ src/main/java/com/example/crud/servlet/BookServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.crud.servlet;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.crud.domain.Domain;
+import com.example.crud.repository.BookRepository;
+
+@Service
+public class BookServiceImpl implements BookService{
+	@Autowired
+	private BookRepository bookRepository;
+	
+	public List<Domain> getAllBookList(){
+	
+		
+		
+		return bookRepository.getAllBookList();
+	}
+}
